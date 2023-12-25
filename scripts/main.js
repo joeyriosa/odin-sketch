6b5dc4cc0cd50e6ebf5a793eb88b4929b59ae3c9
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2023-12-25 17:35:39 +0800

    Add final version of Etch-A-Sketch project

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ let boxWidth = 25;
 
 const container = document.querySelector('#container');
 const btnClear = document.querySelector('#btnClear');
+const chkColor = document.querySelector('#chkColor');
+const chkProgressive = document.querySelector('#chkProgressive');
 
 let isMousePressed = false;
 
@@ -11,19 +13,23 @@ function drawBox() {
     let smallBox = document.createElement('div');
     smallBox.classList.add('smallBox');
     boxWidth = containerWidth / boxesPerLine;
-    console.log(boxWidth);
     smallBox.style.width = boxWidth + 'px';
     smallBox.style.height = boxWidth + 'px';
+    smallBox.style.backgroundColor = 'white';
 
     smallBox.addEventListener('mousedown', (event) => {
         event.preventDefault();
         isMousePressed = true;
-        smallBox.style.backgroundColor = 'black';
+        getOpacity(smallBox);
+        let colorToUse = getColorToUse(smallBox);
+        smallBox.style.backgroundColor = colorToUse;
     });
 
-    smallBox.addEventListener('mousemove', (event) => {
+    smallBox.addEventListener('mouseenter', (event) => {
         if (isMousePressed && event.buttons === 1) {
-            smallBox.style.backgroundColor = 'black';
+            getOpacity(smallBox);
+            let colorToUse = getColorToUse(smallBox);
+            smallBox.style.backgroundColor = colorToUse;
         }
     });
 
@@ -34,10 +40,47 @@ function drawBox() {
     container.appendChild(smallBox);
 }
 
+function getColorToUse(smallBox) {
+    if (chkColor.checked) {
+        // If Progressive, use the current color of the box.
+        if (chkProgressive.checked) {
+            let computedStyle = window.getComputedStyle(smallBox);
+            if (computedStyle.backgroundColor.toLowerCase() != 'rgb(255, 255, 255)') {
+                return computedStyle.backgroundColor;
+            }
+        }
+
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        var returnValue = "rgb(" + red + "," + green + "," + blue + ")";
+        return returnValue;
+    }
+    else {
+        return 'black';
+    }
+}
+
+function getOpacity(smallBox) {
+    if (chkProgressive.checked) {
+        let computedStyle = window.getComputedStyle(smallBox);
+        let currentOpacity = parseFloat(computedStyle.opacity);
+        let currentBackgroundColor = computedStyle.backgroundColor;
+        if (currentBackgroundColor.toLowerCase() == 'rgb(255, 255, 255)') {
+            currentOpacity = 0;
+        }
+        smallBox.style.opacity = currentOpacity + 0.1;
+    }
+    else {
+        smallBox.style.opacity = 1;
+    }
+}
+
 function resetColors() {
     var boxes = document.querySelectorAll('.smallBox');
     boxes.forEach(function (box) {
         box.style.backgroundColor = 'white';
+        box.style.opacity = 1;
     })
 }
 
